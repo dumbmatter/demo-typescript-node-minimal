cdc0d1233476d92bb2e64de6b79417d69f31bdad
Jeremy Scheff <jdscheff@gmail.com>
2015-09-18 17:54:39 -0400

    My best attempt at importing pubsub-js in TypeScript

--- index.ts
+++ index.ts
@@ -3,9 +3,11 @@
 // link the typing, it will export defintions for an 'xregexp' module
 
 /// <reference path="./typings/xregexp/xregexp.d.ts" />
+/// <reference path="./typings/pubsubjs/pubsub.d.ts" />
 
 // import the npm module (using package name)
 import xregexp = require('xregexp');
+import PubSub = require('pubsub-js');
 
 // import our own TypeScript file (using relative path, no extension)
 import greeter = require('./lib/greeter');
